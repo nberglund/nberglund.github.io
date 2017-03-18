482ed365c1a9113dd249edb5eecaceb15d7b59db
Niels Berglund <niels.it.berglund@gmail.com>
2017-03-18 19:49:58 +0200

    New blog-post - SQL Server R Internals I

--- code/sql_r_services_internal/exec_ext_script.sql
+++ code/sql_r_services_internal/exec_ext_script.sql
@@ -6,5 +6,8 @@ EXEC sp_execute_external_script  @language =N'R',
                                  @script=N'OutputDataSet<-InputDataSet',
                                  @input_data_1 =N'SELECT 42'
       WITH RESULT SETS (([TheAnswer] int not null));
-
 GO
+
+sp_helptext sp_execute_external_script
+
+select * from sys.extended_procedures
