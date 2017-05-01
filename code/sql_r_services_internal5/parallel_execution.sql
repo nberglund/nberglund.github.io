abf04f5132e7f7e43449820fc79100283f5ece27
Niels Berglund <niels.it.berglund@gmail.com>
2017-05-01 19:35:00 +0200

    New blog-post - SQL Server R Internals V

--- code/sql_r_services_internal5/parallel_execution.sql
+++ code/sql_r_services_internal5/parallel_execution.sql
@@ -1,20 +1,11 @@
 
 
-USE TestParallel2;
+USE TestParallel;
 GO
 
 SET NOCOUNT ON;
 GO
 
-
-
-
-
---SELECT COUNT(*) FROM dbo.rand_50M
-
---@parallel = 1 no MOD
---16 processes
---8 rows
 EXEC sp_execute_external_script
 	@language = N'R'
 	, @script = N'
@@ -40,87 +31,11 @@ WITH RESULT SETS (
 	)
 ); 
 
---9068 4.94337486948536
---@parallel = 1 no MOD
---1 processes
---1 rows
-EXEC sp_execute_external_script
-	@language = N'R'
-	, @script = N'
-		Sys.sleep(45)
-    pid <- Sys.getpid()
-		r <- rxLinMod(y ~ rand1 + rand2 + rand3 + rand4 + rand5, data=InputDataSet, verbose=1)
-		# print(r)
-		
-		coef <- r$coefficients
-		OutputDataSet <- data.frame(pid=pid, nRows=r$nValidObs, intercept=coef[1])
-	'
-	, @parallel=1
-	, @input_data_1 = N'
-			SELECT TOP(16) * FROM rand_10M'
-	-- , @params = N'@r_rowsPerRead int'
-	-- , @r_rowsPerRead = 100000
-WITH RESULT SETS (
-	(pid INT NOT NULL,
-		nRows INT NOT NULL,
-		intercept FLOAT NULL
-	)
-); 
-
---@parallel = 1 no MOD
---1 processes
---1 rows
-EXEC sp_execute_external_script
-	@language = N'R'
-	, @script = N'
-		Sys.sleep(5)
-    pid <- Sys.getpid()
-		r <- rxLinMod(y ~ rand1 + rand2 + rand3 + rand4 + rand5, data=InputDataSet, verbose=1)
-		# print(r)
-		
-		coef <- r$coefficients
-		OutputDataSet <- data.frame(pid=pid, nRows=r$nValidObs, intercept=coef[1])
-	'
-	, @parallel=1
-	, @input_data_1 = N'
-			SELECT TOP(10000) y, rand1, rand2, rand3, rand4, rand5 FROM rand_10M'
-	-- , @params = N'@r_rowsPerRead int'
-	-- , @r_rowsPerRead = 100000
-WITH RESULT SETS (
-	(pid INT NOT NULL,
-		nRows INT NOT NULL,
-		intercept FLOAT NULL
-	)
-); 
-
-
-SELECT TOP(16) * FROM rand_10M
-
-
-
-EXEC sp_execute_external_script 
-                    @language =N'R',
-                    @script=N'
-                    Sys.sleep(5);
-                    pid <- Sys.getpid()
-                    wd <- getwd()
-                    data2<-InputDataSet
-                    data <- data.frame(Answer=42, pid=pid, wd=wd)
-                    OutputDataSet<-data;
-                    '
-                    --, @parallel=1
-                    , @input_data_1 =N'SELECT * FROM dbo.rand_10M'
-WITH RESULT SETS (([TheAnswer] int not null, ProcessID int, WorkingDirectory nvarchar(1024)));
-
-
-
-SELECT TOP(10) * FROM dbo.rand_50M
-
 DECLARE @start datetime2 = SYSUTCDATETIME();
 EXEC sp_execute_external_script
           @language = N'R'
         , @script = N'
-            Sys.sleep(30)
+            #Sys.sleep(30)
              pid <- Sys.getpid()
              r <- rxLinMod(y ~ rand1 + rand2 + rand3 + rand4 + rand5, 
                            data=InputDataSet)
@@ -132,44 +47,10 @@ EXEC sp_execute_external_script
         , @input_data_1 = N'
               SELECT  y, rand1, rand2, rand3, 
                       rand4, rand5 
-              FROM dbo.rand_30M WHERE RowID < 3000000  OPTION(MAXDOP 4)'
+              FROM dbo.rand_30M'
         , @parallel=1
         --, @params = N'@r_rowsPerRead int'
 	      --, @r_rowsPerRead = 100000
 WITH RESULT SETS ((pid INT NOT NULL, nRows INT NOT NULL, intercept FLOAT NULL)
 ); 
 SELECT DATEDIFF(ms, @start, SYSUTCDATETIME()) AS ElapsedTime;
-
-
-SELECT * 
-FROM sys.dm_os_schedulers 
-WHERE scheduler_id < 255; 
-
-sp_configure 'show advanced options', 1
-RECONFIGURE
-GO
-
-
-SELECT * FROM sys.dm_os_tasks
-SELECT * FROM sys.dm_os_threads
-SELECT * FROM sys.dm_os_workers
-SELECT * FROM sys.dm_os_schedulers
-SELECT * FROM sys.dm_exec_sessions
-
-
-SELECT  
-    task_address,  
-    task_state,  
-    context_switches_count,  
-    pending_io_count,  
-    pending_io_byte_count,  
-    pending_io_byte_average,  
-    scheduler_id,  
-    session_id,  
-    exec_context_id,  
-    request_id,  
-    worker_address,  
-    host_address  
-  FROM sys.dm_os_tasks 
-  WHERE session_id = 54
-  ORDER BY session_id, request_id;  
--- code/sql_r_services_internal5/setup_parallell.sql
+++ code/sql_r_services_internal5/setup_parallell.sql
@@ -14,37 +14,6 @@ GO
 USE TestParallel;
 GO
 
-DROP TABLE IF EXISTS dbo.rand_1M
-CREATE TABLE dbo.rand_1M(RowID bigint identity primary key, y int, rand1 int, rand2 int, rand3 int, rand4 int, rand5 int);
-
-INSERT INTO dbo.rand_1M(y, rand1, rand2, rand3, rand4, rand5)
-SELECT TOP(1000000) CAST(ABS(CHECKSUM(NEWID())) % 14 AS INT) 
-	, CAST(ABS(CHECKSUM(NEWID())) % 20 AS INT)
-	, CAST(ABS(CHECKSUM(NEWID())) % 25 AS INT)
-	, CAST(ABS(CHECKSUM(NEWID())) % 14 AS INT)
-	, CAST(ABS(CHECKSUM(NEWID())) % 50 AS INT)
-	, CAST(ABS(CHECKSUM(NEWID())) % 100 AS INT)
-FROM sys.objects o1
-CROSS JOIN sys.objects o2
-CROSS JOIN sys.objects o3
-CROSS JOIN sys.objects o4;
-
-
-DROP TABLE IF EXISTS dbo.rand_10M
-CREATE TABLE dbo.rand_10M(RowID bigint identity primary key, y int, rand1 int, rand2 int, rand3 int, rand4 int, rand5 int);
-
-INSERT INTO dbo.rand_10M(y, rand1, rand2, rand3, rand4, rand5)
-SELECT TOP(10000000) CAST(ABS(CHECKSUM(NEWID())) % 14 AS INT) 
-	, CAST(ABS(CHECKSUM(NEWID())) % 20 AS INT)
-	, CAST(ABS(CHECKSUM(NEWID())) % 25 AS INT)
-	, CAST(ABS(CHECKSUM(NEWID())) % 14 AS INT)
-	, CAST(ABS(CHECKSUM(NEWID())) % 50 AS INT)
-	, CAST(ABS(CHECKSUM(NEWID())) % 100 AS INT)
-FROM sys.objects o1
-CROSS JOIN sys.objects o2
-CROSS JOIN sys.objects o3
-CROSS JOIN sys.objects o4;
-
 DROP TABLE IF EXISTS dbo.rand_30M
 CREATE TABLE dbo.rand_30M(RowID bigint identity primary key, y int, rand1 int, rand2 int, rand3 int, rand4 int, rand5 int);
 
@@ -61,34 +30,4 @@ CROSS JOIN sys.objects o3
 CROSS JOIN sys.objects o4;
 GO
 
-/*
 
-SELECT COUNT(*) FROM dbo.rand_1M
-SELECT COUNT(*) FROM dbo.rand_10M
-SELECT COUNT(*) FROM dbo.rand_50M
-*/
-
-
-SELECT  
-    task_address,  
-    task_state,  
-    context_switches_count,  
-    pending_io_count,  
-    pending_io_byte_count,  
-    pending_io_byte_average,  
-    scheduler_id,  
-    session_id,  
-    exec_context_id,  
-    request_id,  
-    worker_address,  
-    host_address  
-  FROM sys.dm_os_tasks 
-  WHERE session_id = 54
-    AND scheduler_id < 255;
-
-
-SELECT * 
-FROM sys.dm_os_tasks ot
-JOIN sys.dm_os_workers w
-  ON ot.task_address = w.task_address
-WHERE session_id = 52
