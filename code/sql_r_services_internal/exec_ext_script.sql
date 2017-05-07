54f0ac138fd0ee3c0c06f98b82a8f7dd47820dcd
Niels Berglund <niels.it.berglund@gmail.com>
2017-05-07 21:23:55 +0200

    New blog-post - Roundup Week 18

--- code/sql_r_services_internal/exec_ext_script.sql
+++ code/sql_r_services_internal/exec_ext_script.sql
@@ -12,7 +12,9 @@ GO
 
 EXEC sp_execute_external_script  @language =N'R',
                                  @script=N'
-								 OutputDataSet<-InputDataSet
+								 Sys.sleep(20)
+                 OutputDataSet<-InputDataSet
+                 Sys.Sleepx(5)
 								 ',
                                  @input_data_1 =N'SELECT 42'
       WITH RESULT SETS (([TheAnswer] int not null));
